73c2eb439cee2c610e4533d24862ee8104307935
AkThao <akaash.thao@gmail.com>
2020-09-30 15:57:32 +0100

    Updates

---
+++ precache-manifest.fa75e7e814443f529c545ba442fbf46a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e66742be169a869d45da879bd7b4dc2c",
+    "url": "/complementary-colours/index.html"
+  },
+  {
+    "revision": "c1619e480bd52f4e330f",
+    "url": "/complementary-colours/static/css/main.82323c69.chunk.css"
+  },
+  {
+    "revision": "e5ae4e238ecd8739af07",
+    "url": "/complementary-colours/static/js/2.9fb03c99.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/complementary-colours/static/js/2.9fb03c99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1619e480bd52f4e330f",
+    "url": "/complementary-colours/static/js/main.5b9f4856.chunk.js"
+  },
+  {
+    "revision": "8bf3f03e6d144274144a",
+    "url": "/complementary-colours/static/js/runtime-main.19e3d7e9.js"
+  }
+]);
